008e9eb900c3691bf16ced180db17e14d123c3b5
Molly Wu <WuMengting93@outlook.com>
2023-04-21 22:52:47 +0800

    修改baseURL

--- src/config/global.js
+++ src/config/global.js
@@ -1,6 +1,7 @@
 // const BASE_URL = 'https://2de0f6ba.r5.vip.cpolar.cn'
 // const BASE_URL = 'http://localhost:8888/'
-const BASE_URL = 'http://localhost:8080/'
+const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://192.168.31.230:8080'
 export default {
   BASE_URL
 }
